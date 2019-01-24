e99c1c4c4d25738feb292bf302f1791c9f4d98c8
usaidsherwani <uk.sherwani2@gmail.com>
2019-01-24 14:52:16 +0500

    Quiz Searching 8

--- pro/register.php
+++ pro/register.php
@@ -11,6 +11,24 @@ require "server/functions.php";
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Old+Standard+TT">
+    <script>
+        function checkEmail(str) {
+            if (str.length == 0) {
+                document.getElementById("hint").innerHTML = "";
+                return;
+            } else {
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        document.getElementById("hint").innerHTML = this.responseText;
+                    }
+                };
+                xmlhttp.open("GET", "check_email.php?e=" + str, true);
+                xmlhttp.send();
+                //document.getElementById('hint').innerHTML = 'loading...';
+            }
+        }
+    </script>
 </head>
 <body>
 
@@ -106,7 +124,7 @@ require "server/functions.php";
                             <div class="input-group-text"><i class="fas fa-user"></i></div>
                         </div>
                         <input type="text" class="form-control" id="c_name" name="c_name" placeholder="Enter your name"
-                               required pattern="0?[1-9]|1[0-9]|2[0-9]|3[0-1]">
+                               required pattern="abc">
                     </div>
                 </div>
                 <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
@@ -117,8 +135,9 @@ require "server/functions.php";
                         <div class="input-group-prepend">
                             <div class="input-group-text"><i class="fas fa-at"></i></div>
                         </div>
-                        <input type="text" class="form-control" id="c_email" name="c_email" placeholder="Enter your date" 
-								required pattern="19[5-9][0-9]|20[0-1]">
+                        <input type="text" class="form-control" id="c_email" name="c_email" placeholder="Enter your email"
+                               onkeyup="checkEmail(this.value)">
+                        <span class="text-danger" id="hint"></span>
                     </div>
                 </div>
             </div>
---
+++ pro/search.php
@@ -0,0 +1,27 @@
+<?php
+require "server/functions.php";
+$e = $_REQUEST["e"];
+$sel_product = "select * from products where pro_keywords like '%$e%'";
+$run_pro  = mysqli_query($con,$sel_product);
+$count = mysqli_num_rows($run_pro);
+if($count>0){
+    while($row = mysqli_fetch_assoc($run_pro)){
+        $pro_id = $row['pro_id'];
+        $pro_title = $row['pro_title'];
+        $pro_price = $row['pro_price'];
+        $pro_image = $row['pro_image'];
+        echo "
+                <div class='col-sm-6 col-md-4 col-lg-3 text-center product-summary'>
+                    <h5 class='text-capitalize'>$pro_title</h5>
+                    <img src='admin/product_images/$pro_image'>
+                    <p> <b> Rs $pro_price/-  </b> </p>
+                    <a href='detail.php' class='btn btn-info btn-sm'>Details</a>
+                    <a href='#'>
+                        <button class='btn btn-primary btn-sm'>
+                            <i class='fas fa-cart-plus'></i> Add to Cart
+                        </button>
+                    </a>
+                </div>
+        ";
+    }
+}.
